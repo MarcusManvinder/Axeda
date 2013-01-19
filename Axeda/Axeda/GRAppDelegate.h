98129e2c3fefd00979d28e651568c4dfd64d8b93
Gaurav <gaurav.raj@me.com>
2013-01-19 12:52:11 +0530

    First Commit...

---
+++ Axeda/Axeda/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  Axeda
+//
+//  Created by Gaurav on 19/01/13.
+//  Copyright (c) 2013 Gaurav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Axeda/Axeda/GRViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRViewController.h
+//  Axeda
+//
+//  Created by Gaurav on 19/01/13.
+//  Copyright (c) 2013 Gaurav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRViewController : UIViewController
+
+@end
